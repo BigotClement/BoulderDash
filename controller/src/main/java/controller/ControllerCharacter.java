9672e6d1828ace32aa6d63163b9bc7a4f9deb865
gautier-perrot <44995382+gautier-perrot@users.noreply.github.com>
2019-05-30 13:51:29 +0200

    Diamond Count

--- controller/src/main/java/controller/ControllerCharacter.java
+++ controller/src/main/java/controller/ControllerCharacter.java
@@ -7,8 +7,19 @@ import java.awt.event.KeyEvent;
 
 import contract.IModel;
 import contract.IView;
+import mobile.MobileEntityFactory;
 
 public class ControllerCharacter extends Controller {
+
+    private int diamondCount = 0;
+
+    public int getDiamondCount() {
+        return this.diamondCount;
+    }
+
+    public void setDiamondCount(int diamondCount) {
+        this.diamondCount = diamondCount;
+    }
 
     public ControllerCharacter(IView view, IModel model) {
         super(view, model);
@@ -20,25 +31,51 @@ public class ControllerCharacter extends Controller {
 
     @Override
     public void moveSet(KeyEvent key) {
+
         switch (key.getKeyChar()) {
             case 'z':
+                if (this.getModel().getMap().getMap()[this.getModel().getMap().findCharacter().getY() - 1][this
+                        .getModel().getMap().findCharacter().getX()].getClass() == MobileEntityFactory.createDiamond()
+                                .getClass()) {
+                    this.incrementDiamond();
+                }
                 this.moveUp(this.getModel().getMap().findCharacter());
                 this.getModel().getMap().findCharacter().setSpriteFolder("sprites\\Mobile\\Character\\Up");
                 break;
             case 'q':
+                if (this.getModel().getMap().getMap()[this.getModel().getMap().findCharacter().getY()][this.getModel()
+                        .getMap().findCharacter().getX() - 1].getClass() == MobileEntityFactory.createDiamond()
+                                .getClass()) {
+                    this.incrementDiamond();
+                }
                 this.moveLeft(this.getModel().getMap().findCharacter());
                 this.getModel().getMap().findCharacter().setSpriteFolder("sprites\\Mobile\\Character\\Left");
                 break;
             case 's':
+                if (this.getModel().getMap().getMap()[this.getModel().getMap().findCharacter().getY() + 1][this
+                        .getModel().getMap().findCharacter().getX()].getClass() == MobileEntityFactory.createDiamond()
+                                .getClass()) {
+                    this.incrementDiamond();
+                }
                 this.moveDown(this.getModel().getMap().findCharacter());
                 this.getModel().getMap().findCharacter().setSpriteFolder("sprites\\Mobile\\Character\\Down");
                 break;
             case 'd':
+                if (this.getModel().getMap().getMap()[this.getModel().getMap().findCharacter().getY()][this.getModel()
+                        .getMap().findCharacter().getX() + 1].getClass() == MobileEntityFactory.createDiamond()
+                                .getClass()) {
+                    this.incrementDiamond();
+                }
                 this.moveRight(this.getModel().getMap().findCharacter());
                 this.getModel().getMap().findCharacter().setSpriteFolder("sprites\\Mobile\\Character\\Right");
                 break;
             default:
                 break;
         }
+    }
+
+    private void incrementDiamond() {
+        this.setDiamondCount(this.getDiamondCount() + 1);
+        System.out.println(this.getDiamondCount());
     }
 }
