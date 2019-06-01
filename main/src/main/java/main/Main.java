27f64caeeb056ac38ae1cd805d3b84c86b627a51
Gildur7161 <amaurychabane@gmail.com>
2019-06-01 16:25:32 +0200

    menu at program start

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -3,13 +3,13 @@
  */
 package main;
 
+import javax.swing.JOptionPane;
+
 import controller.ControllerMain;
 import model.Model;
 import view.View;
 
 public abstract class Main {
-
-    private static final int MAPID = 3;
 
     private static final int DIAMONDLIMIT = 10;
 
@@ -22,7 +22,8 @@ public abstract class Main {
      *                 the arguments
      */
     public static void main(final String[] args) {
-        final Model model = new Model(MAPID);
+        final Model model = new Model(
+                Integer.parseInt(JOptionPane.showInputDialog("Choose a mapID (currently implemented : 1,2,3,4,5)")));
         final View view = new View(model);
         final ControllerMain controller = new ControllerMain(view, model);
         controller.getControllerCharacter().setDiamondLimit(DIAMONDLIMIT);
