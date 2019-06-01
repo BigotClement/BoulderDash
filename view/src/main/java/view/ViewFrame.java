2bd2e17f90d1e2284427a9a9ffe170def05ab586
Gildur7161 <amaurychabane@gmail.com>
2019-06-01 16:51:51 +0200

    volume / 2

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
@@ -58,6 +59,10 @@ public class ViewFrame extends JFrame implements KeyListener {
             audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            double gain = 0.5;
+            float dB = (float) ((Math.log(gain) / Math.log(10.0)) * 20.0);
+            gainControl.setValue(dB);
             clip.loop(Clip.LOOP_CONTINUOUSLY);
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             // TODO Auto-generated catch block
