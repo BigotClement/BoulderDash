1bc39ac093e3b3b57b6d38df5981893e4b5f8c81
Tintouin <50229493+Tintouin@users.noreply.github.com>
2019-06-01 23:20:52 +0200

    test viewpanel

--- view/src/test/java/view/ViewPanelTest.java
+++ view/src/test/java/view/ViewPanelTest.java
@@ -6,6 +6,11 @@ package view;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,6 +37,38 @@ public class ViewPanelTest {
     @Test
     public void testViewPanel() {
         assertNotNull(this.viewPanel);
+    }
+
+    @Test
+    public void testGetDiamondCounterImage() {
+        assertNull(this.viewPanel.getDiamondCounterImage());
+    }
+
+    @Test
+    public void testSetDiamondCounterImage() {
+        try {
+            this.viewPanel.setDiamondCounterImage(ImageIO.read(new File("diamondcounter.png")));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        assertNotNull(this.viewPanel.getDiamondCounterImage());
+    }
+
+    @Test
+    public void testGetTimerImage() {
+        assertNull(this.viewPanel.getTimerImage());
+    }
+
+    @Test
+    public void testSetTimerImage() {
+        try {
+            this.viewPanel.setTimerImage(ImageIO.read(new File("timer.png")));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        assertNotNull(this.viewPanel.getTimerImage());
     }
 
 }
